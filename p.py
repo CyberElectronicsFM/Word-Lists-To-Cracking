b7b1e355aaaac8c210da73f7678b660fc47a4382
hitlerhack313 <59124860+hitlerhack313@users.noreply.github.com>
2020-06-12 13:23:12 +0000

    Add files via upload

---
+++ p.py
@@ -0,0 +1,109 @@
+import os
+import time
+#####################################################
+b ="\033[0;91m" #RED
+f ="\033[1;92m" #GREEN
+c ="\033[1;96m" #
+l ="\033[1;94m" #
+v ="\033[0;101m" #
+#######################################################
+print ("                                     ")
+M = input ("ENTER YOUR BASSWORD TO START CRACK ==> ")
+if M == 'admin':
+#  or v == 'MINA' or v == 'kaliiiii' :
+   os.system("clear")
+#  time.sleep(0.1)
+   print ("")
+else:
+#   t(0.1,"\n\t            PLEASE WAIT ....")
+    print ("[+] ERORR ? TRY AGAIN [*_*]")
+def lock () :
+    print ("")
+    time.sleep(0.1)
+    print (b+"    =================== YOU CAN GET BASSWORD IN ==================== ")
+    time.sleep(0.1)
+    print (f+"   |             www.m3lomatthephone.blogspot.com                   |")
+    time.sleep(0.1)
+    print (f+"   |     ==>  https://www.youtube.com/watch?v=XX3jp9GVMn8   <==     |")
+    time.sleep(1)
+    print (f+"   |         https://t.me/joinchat/NYmlVhPbCU9tfGxAeSaZ3A           |")
+    time.sleep(0.1)
+    print (f+"   |               (+) THIS TOOL BY MENA MAGDY (+)                  |")
+    time.sleep(0.1)
+    print (b+"    ==================== VISIT US IN THIS ========================== ")
+    time.sleep(0.1)
+    print ("                                                                       ")
+    time.sleep(0.1)
+    print (b+ "    --------------------------------------------------------------")
+    time.sleep(0.1)
+    print (f+"   |         GO TO ===>> www.m3lomatthephone.blogspot.com          |")
+    time.sleep(0.1)
+    print (b+"    ---------------------------------------------------------------")
+    print ("")
+    time.sleep(10)
+    os. system("clear")
+    os.system("python3 p.py")
+    lock()
+########################################################################################################################
+#os. system("clear")
+print (b+"   _________________________________________")
+print (f+"    WELCOME TO HASH CRACK WPA/WPA2 PSK & WPA")
+print (b+"   -----------------------------------------")
+os.system("figlet     WELCOME")
+os.system("figlet SUBISCRIBE ON MY CHANEL *_*")
+#os.system("aircrack-ng -a2 -w  ")
+############################################# WPA/WPA2 PSK ###############################################################
+
+# [ 1 ]   ############################
+os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list1.txt /home/kali/Desktop/HITLER-PNTURWIFI/wpa.cap")
+
+##############################
+print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+print ("                                 ")
+kal3 = input ("WRITE YOUR OPTION ==> ")
+if kal3 == "1" :
+   os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list2.txt /home/kali/Desktop/HITLER-PNTURWIFI/wpa.cap")
+
+#############################################################################################################################################################
+print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+print ("                                 ")
+kal4 = input ("WRITE TOYR OPTION ==>")
+if kal4 == "1" :
+   os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list3.txt /home/kali/Desktop/HITLER-PNTURWIFI/wpa.cap")
+
+###########################
+print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+print ("                                 ")
+kal5 = input ("WRITE YOUR OPTION ==> ")
+if kal5 == "1" :
+   os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list4.txt /home/kali/Desktop/HITLER-PNTURWIFI/wpa.cap")
+
+#######################################################################################################################
+
+print ("============================== GO TO CRACK WEP NETWORKS =========================================")
+
+################################################ WEP PSK #################################################################
+os.system("figlet wep2 psk cracking")
+###########################################
+#print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+#print ("                                 ")
+#kal6 = input ("WRITE TOYR OPTION ==>")
+###########################################
+os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list1.txt /home/kali/Desktop/HITLER-PNTURWIFI/wep.cap")
+print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+print ("                                 ")
+kal7 = input ("WRITE TOYR OPTION ==>")
+############################################
+os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list2.txt /home/kali/Desktop/HITLER-PNTURWIFI/wep.cap")
+print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+print ("                                 ")
+kal8 = input ("WRITE TOYR OPTION ==>")
+############################################
+os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list3.txt /home/kali/Desktop/HITLER-PNTURWIFI/wep.cap")
+print (" (+_+) PRESS 1 TO CINTUNIO CRACK ")
+print ("                                 ")
+kal9 = input ("WRITE TOYR OPTION ==>")
+os.system("aircrack-ng -a2 -w /home/kali/Desktop/HITLER-PNTURWIFI/list4.txt /home/kali/Desktop/HITLER-PNTURWIFI/wep.cap")
+##########################################################################################################################
+##########################################################################################################################
+##########################################################################################################################
